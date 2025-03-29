8758a167f06827cfb50f6ce485e40c9372ff8f72
BekzhanK1 <bkimadieff@gmail.com>
2025-03-29 12:11:51 +0500

    .env

--- webproject/src/utils/config.js
+++ webproject/src/utils/config.js
@@ -1,5 +1,1 @@
-// export const API_URL = "https://api.protosedu.kz/api";
-export const API_URL = "https://api.protosedu.kz/api";
-// export const API_URL = "http://localhost:8000/api";
-// export const API_URL = "http://localhost:9000/api";
-// export const API_URL = "http://localhost:9000/api";
+export const API_URL = import.meta.env.VITE_API_URL;
