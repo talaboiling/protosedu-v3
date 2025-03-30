488c496917145971d2368f11899e14c7dc48a39b
BekzhanK1 <bkimadieff@gmail.com>
2025-03-30 12:30:25 +0500

    tasks done only in order

--- webproject/src/pages/courses/contents/SectionContent.jsx
+++ webproject/src/pages/courses/contents/SectionContent.jsx
@@ -20,8 +20,9 @@ const SectionContent = ({
   const baseRowHeight = 90;
   const xOffset = 220;
   const yOffset = 150;
+  let isBlocked = false;
 
-  if (chapter.contents){
+  if (chapter.contents) {
     containerHeight = chapter.contents.length * 160;
   }
 
@@ -55,108 +56,116 @@ const SectionContent = ({
             margin: "0 auto",
           }}
         >
-        <div className="lessonsLinks">
-          {chapter.contents && chapter.contents.map((content, contentIndex) => {
-            const row = Math.floor(contentIndex / 2);
-            const col = contentIndex % 2;
-            const top = contentIndex * yOffset;
+          <div className="lessonsLinks">
+            {chapter.contents && chapter.contents.map((content, contentIndex) => {
+              const isTask = content.content_type === "task";
+              const isLesson = content.content_type === "lesson";
+              const row = Math.floor(contentIndex / 2);
+              const col = contentIndex % 2;
+              const top = contentIndex * yOffset;
 
-            const isEvenRow = row % 2 === 0;
-            let style = {
-              position: "absolute",
-              top: top,
-              width: itemWidth,
-              height: itemWidth,
-              backgroundColor: "#ccc",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              overflowY: "hidden"
-            };
+              let isDisabled = false;
 
-            if (isEvenRow) {
-              style.left = col * xOffset;
-              console.log(style.left);
-            } else {
-              style.left = containerWidth - itemWidth - col * xOffset;
-            }
-            return (
-                <>
-                {content.content_type === "lesson" ? (
-                  <div
-                    className={`vidBlock studVidBlock ${
-                      hasSubscription ? "" : "noVidBlock"
-                    }`}
-                    onClick={() =>
-                      hasSubscription
-                        ? openVideoModal(content.video_url)
-                        : setShowSubscriptionError(true)
-                    }
-                    style={style}
-                    key={contentIndex}
-                  >
-                    <div className="thumbcontainer">
+              if (isTask) {
+                if (isBlocked) {
+                  isDisabled = true;
+                }
+                if (!content.is_completed) {
+                  isBlocked = true;
+                }
+              }
+
+              const isEvenRow = row % 2 === 0;
+              let style = {
+                position: "absolute",
+                top: top,
+                width: itemWidth,
+                height: itemWidth,
+                backgroundColor: "#ccc",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                overflowY: "hidden"
+              };
+
+              if (isEvenRow) {
+                style.left = col * xOffset;
+              } else {
+                style.left = containerWidth - itemWidth - col * xOffset;
+              }
+
+              return (
+                <div key={contentIndex}>
+                  {isLesson ? (
+                    <div
+                      className={`vidBlock studVidBlock ${hasSubscription ? "" : "noVidBlock"}`}
+                      onClick={() =>
+                        hasSubscription
+                          ? openVideoModal(content.video_url)
+                          : setShowSubscriptionError(true)
+                      }
+                      style={style}
+                    >
+                      <div className="thumbcontainer">
+                        <img
+                          src={bgvideo || "placeholder.png"}
+                          alt="vidname"
+                          className="taskThumbnail"
+                        />
+                      </div>
+                      <p
+                        style={{
+                          backgroundColor: "white",
+                          margin: "0",
+                          padding: "7px 40px",
+                          borderRadius: "10px",
+                        }}
+                      >
+                        {content.title}
+                      </p>
+                    </div>
+                  ) : (
+                    <div
+                      className={`studVidBlock task ${hasSubscription && !isDisabled ? "" : "noTask"}`}
+                      onClick={() =>
+                        hasSubscription && !isDisabled
+                          ? openTaskModal(content.id)
+                          : setShowSubscriptionError(true)
+                      }
+                      style={{ ...style, backgroundColor: "#97d4e7", opacity: isDisabled ? 0.5 : 1 }}
+                    >
                       <img
-                        src={bgvideo || "placeholder.png"}
+                        src={bgtask || "placeholder.png"}
                         alt="vidname"
                         className="taskThumbnail"
                       />
+                      <p
+                        style={{
+                          backgroundColor: "white",
+                          margin: "0",
+                          padding: "7px 40px",
+                          borderRadius: "10px",
+                          marginBottom: "7px",
+                        }}
+                      >
+                        {content.title}
+                      </p>
+                      {content.is_completed ? (
+                        <div className="completedTask">
+                          <VerifiedIcon sx={{ color: "#19a5fc" }} />
+                          <strong>{t("youCompletedTask")}</strong>
+                        </div>
+                      ) : (
+                        <div className="completedTask incompleteTask">
+                          <strong>{t("youHaveNewTask")}</strong>
+                        </div>
+                      )}
                     </div>
-                    <p
-                      style={{
-                        backgroundColor: "white",
-                        margin: "0",
-                        padding: "7px 40px",
-                        borderRadius: "10px",
-                      }}
-                    >
-                      {content.title}
-                    </p>
-                  </div>
-                ) : (
-                  <div
-                    className={`studVidBlock task ${
-                      hasSubscription ? "" : "noTask"
-                    }`}
-                    onClick={() =>
-                      hasSubscription
-                        ? openTaskModal(content.id)
-                        : setShowSubscriptionError(true)
-                    }
-                    style={{...style, backgroundColor: "#97d4e7"}}
-                    key={contentIndex}
-                  >
-                    <img
-                      src={bgtask || "placeholder.png"}
-                      alt="vidname"
-                      className="taskThumbnail"
-                    />
-                    <p
-                      style={{
-                        backgroundColor: "white",
-                        margin: "0",
-                        padding: "7px 40px",
-                        borderRadius: "10px",
-                        marginBottom: "7px",
-                      }}
-                    >
-                      {content.title}
-                    </p>
-                    {content.is_completed ? (
-                      <div className="completedTask">
-                        <VerifiedIcon sx={{ color: "#19a5fc" }} />
-                        <strong>{t("youCompletedTask")}</strong>
-                      </div>
-                    ) : (
-                      <div className="completedTask incompleteTask">
-                        <strong>{t("youHaveNewTask")}</strong>
-                      </div>
-                    )}
-                  </div>
-                )}
-                </>
-            );
-          })}
+                  )}
+                </div>
+              );
+            })}
+
           </div>
         </div>
       </div>
