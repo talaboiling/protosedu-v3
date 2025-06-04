0b647d2f5c98cd58e8139eb11ff1e9cae0baeefb
AlikhanNasa7 <alihannashtaj@gmail.com>
2025-06-05 01:00:37 +0500

    Preview image logic

--- webproject/src/pages/dashboard_sections/tests/Answers.jsx
+++ webproject/src/pages/dashboard_sections/tests/Answers.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { Maximize2 } from 'lucide-react';
+import React, { useState } from 'react'
+import classes from './Answers.module.css'
+import Modal from '../../../helpers/Modal';
+import HPBs_Form from './HPBs_Form';
 
 const Answers = ({answers,selectedAnswer,answerState, onSelect, index, mode, isCorrect}) => {
     console.log(selectedAnswer, answers);
+
+    const [showModal, setShowModal] = useState(false);
+    const [answerImage, setAnswerImage] = useState(null);
+    const [answerIndex, setAnswerIndex] = useState(null);
+
+    function viewImageHandler(e, answerImage, answerIndex){
+        e.stopPropagation();
+        setShowModal(true);
+        setAnswerImage(answerImage);
+        setAnswerIndex(answerIndex);
+    }
+
     return (
         <ul id='answers'>
             {answers.map(answer=>{
@@ -55,14 +71,38 @@ const Answers = ({answers,selectedAnswer,answerState, onSelect, index, mode, isC
                     )
                 }else if (answer.option_type==="image"){
                     return <li key={answer.id} className='answer'>
-                            <button key={answer.id} onClick={()=>onSelect(answer, index)} 
-                                style={{backgroundColor: isSelected ? "green" : ""}}
-                            >
-                                <img src={answer.image} style={{width: "100px"}}/>
-                            </button>
+                        <button key={answer.id}
+                            onClick={()=>onSelect(answer, index)} 
+                            style={{backgroundColor: isSelected ? "green" : "", position:"relative"}}
+                            className={classes['answer-button']}
+                        >
+                            <img src={answer.image} style={{width: "100px"}}/>
+                            <div className={classes['maximize-btn-bg']}>
+                                <Maximize2 
+                                    size={20} 
+                                    className={classes['maximize-btn']} 
+                                    onClick={(e)=>viewImageHandler(e, answer, index)}
+                                />
+                            </div>
+                        </button>
                     </li>
                 }
-                    })}
+            })}
+            {showModal && answerImage && answerIndex && <Modal onClose={()=>{
+                setShowModal(false);
+                setAnswerImage(null);
+            }} extraStyles={{width:"400px"}}>
+                <form style={{width:"100%", display:"flex", 
+                flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+                    <img src={answerImage?.image} style={{width:"100%"}}/>
+                    {(!selectedAnswer || selectedAnswer.image!==answerImage.image) && <button style={{width: "fit-content"}} onClick={()=>onSelect(answerImage, answerIndex)}>
+                        Выбрать как ответ
+                    </button>}
+                    {selectedAnswer && selectedAnswer.image===answerImage.image && <button type='button' style={{width: "fit-content", backgroundColor: "green"}}>
+                        Выбран как ответ
+                    </button>}
+                </form>
+            </Modal>}
         </ul>
     )
 }
